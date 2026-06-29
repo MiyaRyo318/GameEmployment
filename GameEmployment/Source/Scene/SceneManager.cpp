4ca1165acff548a14815c22c38eeae378b9b1e97
MiyaRyo318 <ryoto8262@gmail.com>
2026-06-29 14:49:42 +0900

    d

---
+++ GameEmployment/Source/Scene/SceneManager.cpp
@@ -0,0 +1,27 @@
+#include "SceneManager.h"
+
+void SceneManager::Init()
+{
+    m_Scene = TITLE;
+}
+
+
+void SceneManager::Update()
+{
+
+
+}
+
+
+void SceneManager::Draw()
+{
+
+
+}
+
+
+void SceneManager::End()
+{
+
+
+}
