f3d386e9f8a97ce2d5bcd1ea9759a1b6bedf5be3
MiyaRyo318 <ryoto8262@gmail.com>
2026-09-25 13:29:40 +0900

    teki

--- GameEmployment/Source/Skybox/Skybox.cpp
+++ GameEmployment/Source/Skybox/Skybox.cpp
@@ -55,24 +55,7 @@ void Skybox::Init()
 
 void Skybox::Update()
 {
-    if (m_Model == -1)
-    {
-        return;
-    }
-
-    // 背景を左方向へ移動
-    m_Position.x -= m_ScrollSpeed;
-
-    // 一定距離まで移動したら右側へ戻す
-    if (m_Position.x < -1000.0f)
-    {
-        m_Position.x = 1000.0f;
-    }
-
-    // モデルの位置を更新
-    MV1SetPosition(
-        m_Model,
-        m_Position);
+    
 }
 
 void Skybox::Draw()
